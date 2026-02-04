852ba2fe48d7f2bbefa1658345473677ac402e99
caiohas <caiohas.2908@gmail.com>
2026-02-04 17:36:20 -0300

    adicionando primeiro exemplo e primeiro exercicio

---
+++ aula01/exemplo_00.py
@@ -0,0 +1,4 @@
+print('Hello World!')
+print(5 + 3)
+print('Ola' + ' ' + 'pessoal!')
+print('Olá, ' + input('Digite seu nome:') + '!') 
---
+++ aula01/exercicio_01.py
@@ -0,0 +1,4 @@
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+nome = input('Digite seu nome para calculo do numero de caracteres: ')
+print(f'{nome} possui {len(nome)} caracteres!')
