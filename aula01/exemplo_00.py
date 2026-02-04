07f15e537849a94c2b1a4ddb713366ea8ce45791
caiohas <caiohas.2908@gmail.com>
2026-02-04 17:57:07 -0300

    Adicionando tipos de dados no exemplo_00.py

--- aula01/exemplo_00.py
+++ aula01/exemplo_00.py
@@ -1,4 +1,17 @@
 print('Hello World!')
 print(5 + 3)
 print('Ola' + ' ' + 'pessoal!')
-print('Olá, ' + input('Digite seu nome:') + '!') 
+print('Olá, ' + input('Digite seu nome:' ) + '!') 
+
+# tipos de dados python
+numero = 13 # int
+numero_decimal = 5.89 # float
+nome_usuario = 'Caio' # string
+verdade = True # bool
+falso = False # bool
+
+print(type(numero))
+print(type(numero_decimal))
+print(type(nome_usuario))
+print(type(verdade))
+print(type(falso))
--- aula01/exercicio_02.py
+++ aula01/exercicio_02.py
@@ -1,5 +1,5 @@
 # Criar um programa onde o usuário digite dois valores e apareça a soma
 
-num1 = input('Digite o valor do primeiro numero a ser somado: ')
-num2 = input('Digite o valor do segundo numero a ser somado: ')
-print(f'A soma de {num1} + {num2} = {int(num1) + int(num2)}!')
+num1 = int(input('Digite o valor do primeiro numero a ser somado: '))
+num2 = int(input('Digite o valor do segundo numero a ser somado: '))
+print(f'A soma de {num1} + {num2} = {num1 + num2}!')
