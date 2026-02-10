772fc2ba21fd312e9e1870e5cd39587fb08917dc
caiohas <caiohas.2908@gmail.com>
2026-02-10 09:25:54 -0300

    Inserindo o segundo exercicio da parte 1 inteiros

--- aula02/exercicios_inteiros1.py
+++ aula02/exercicios_inteiros1.py
@@ -2,11 +2,14 @@
 
 # Parte 01: inteiros
 # 1) Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-num1 = int(input('Insira um numero inteiro: '))
-num2 = int(input('Insira um outro numero inteiro: '))
-print(f'A soma dos dois numeros é: {num1 + num2}')
+# num1 = int(input('Insira um numero inteiro: '))
+# num2 = int(input('Insira um outro numero inteiro: '))
+# print(f'A soma dos dois numeros é: {num1 + num2}')
 
 # 2) Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+num = int(input('Insira o numero que tera o resto da divisao por 5 calculado: '))
+print(f'O resto da divisao {num}/5 é: {num % 5}') 
+
 # 3) Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
