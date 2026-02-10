840509ee2a7a93ee09931c29ae55b4bcd04d2c40
caiohas <caiohas.2908@gmail.com>
2026-02-10 09:17:42 -0300

    Inserindo o exercicio 01 da parte de inteiros

---
+++ aula02/exercicios_inteiros1.py
@@ -0,0 +1,12 @@
+## EXERCICIOS AULA 02: TypeError, Type Check, Type Conversion, try-except e if
+
+# Parte 01: inteiros
+# 1) Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+num1 = int(input('Insira um numero inteiro: '))
+num2 = int(input('Insira um outro numero inteiro: '))
+print(f'A soma dos dois numeros é: {num1 + num2}')
+
+# 2) Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 3) Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
