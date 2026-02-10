1a026a0cbbf9d0eee6927b9f8e5fd77687b38eff
caiohas <caiohas.2908@gmail.com>
2026-02-10 16:42:47 -0300

    exercicio 1 lista string

---
+++ aula02/exercicios_3string.py
@@ -0,0 +1,10 @@
+# EXERCICIOS AULA 02: TypeError, Type Check, Type Conversion, try-except e if
+
+# Parte 03: strings
+# 11) Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+string = input('Digite uma frase... ')
+print(f'Sua frase em maiusculo: {string.upper()}')
+# 12) Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# 13) Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 14) Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 15) Escreva um programa que concatene duas strings fornecidas pelo usuário.
