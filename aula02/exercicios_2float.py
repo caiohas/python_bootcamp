a8be56d94f94711b9fd0b9760980d416fc2a8096
caiohas <caiohas.2908@gmail.com>
2026-02-10 16:31:01 -0300

    adicionando exercicioo 3 da lista de float

--- aula02/exercicios_2float.py
+++ aula02/exercicios_2float.py
@@ -7,12 +7,16 @@
 # print(f'A soma dos dois numeros é: {num1 + num2}')
 
 # # 7) Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
-num1 = float(input('Insira um numero float: '))
-num2 = float(input('Insira um outro numero float: '))
-media = float(num1 + num2) / 2
-
-print(f'A media dos dois numeros é: {media}')
+# num1 = float(input('Insira um numero float: '))
+# num2 = float(input('Insira um outro numero float: '))
+# media = float(num1 + num2) / 2
+# print(f'A media dos dois numeros é: {media}')
 
 # # 8) Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+base = float(input('Insira a base da potencia: '))
+expoente = float(input('Insira o expoente da potencia: '))
+potencia = base ** expoente
+print(f'O resultado da potencia de base {base} e {expoente} é: {potencia}')
+
 # # 9) Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # # 10) Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
