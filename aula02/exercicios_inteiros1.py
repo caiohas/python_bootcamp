d68a35355873b07423c9e7a0c592d40de51736b3
caiohas <caiohas.2908@gmail.com>
2026-02-10 09:31:52 -0300

    Inserindo o exercicio 4 da lista 01 inteiros

--- aula02/exercicios_inteiros1.py
+++ aula02/exercicios_inteiros1.py
@@ -11,9 +11,13 @@ num = int(input('Insira o numero que tera o resto da divisao por 5 calculado: ')
 print(f'O resto da divisao {num}/5 é: {num % 5}') 
 
 # 3) Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-num1 = float(input('Insira um numero: '))
-num2 = float(input('Insira um outro numero: '))
+num1 = int(input('Insira um numero inteiro: '))
+num2 = int(input('Insira um outro numero inteiro: '))
 print(f'A multiplicacao dos dois numeros é: {num1 * num2}')
 
 # 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+num1 = int(input('Insira um numero inteiro: '))
+num2 = int(input('Insira um outro numero inteiro: '))
+print(f'A divisao dos dois numeros é: {num1 / num2}')
+
 # 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
