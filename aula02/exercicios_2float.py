cd7f678b7c9d0a040642d664ef5ee194cfd8c0fd
caiohas <caiohas.2908@gmail.com>
2026-02-10 16:26:47 -0300

    adicionando exercicios 1 e 2 da lista de float

---
+++ aula02/exercicios_2float.py
@@ -0,0 +1,18 @@
+# ## EXERCICIOS AULA 02: TypeError, Type Check, Type Conversion, try-except e if
+
+# # Parte 02: float
+# # 6) Escreva um programa que receba dois números flutuantes e realize sua adição.
+# num1 = float(input('Insira um numero float: '))
+# num2 = float(input('Insira um outro numero float: '))
+# print(f'A soma dos dois numeros é: {num1 + num2}')
+
+# # 7) Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+num1 = float(input('Insira um numero float: '))
+num2 = float(input('Insira um outro numero float: '))
+media = float(num1 + num2) / 2
+
+print(f'A media dos dois numeros é: {media}')
+
+# # 8) Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# # 9) Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# # 10) Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
